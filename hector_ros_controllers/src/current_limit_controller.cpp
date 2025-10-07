20c072fb8f48c5ebc249fb467e98638de3537d4e
md26woxu <marekdaniv@googlemail.com>
2025-10-07 16:51:19 +0200

    Move unfinished chained current controller to seperate branch

--- hector_ros_controllers/src/current_limit_controller.cpp
+++ hector_ros_controllers/src/current_limit_controller.cpp
@@ -12,9 +12,13 @@ CurrentLimitController::CurrentLimitController()
 
 controller_interface::CallbackReturn CurrentLimitController::on_init()
 {
-  param_listener_ = std::make_shared<ParamListener>( get_node() );
-  params_ = param_listener_->get_params();
-
+  try {
+    param_listener_ = std::make_shared<ParamListener>( get_node() );
+    params_ = param_listener_->get_params();
+  } catch ( const std::exception &e ) {
+    fprintf( stderr, "Exception thrown during init stage with message: %s \n", e.what() );
+    return controller_interface::CallbackReturn::ERROR;
+  }
   return read_parameters();
 }
 
