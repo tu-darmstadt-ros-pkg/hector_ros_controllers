b4116334f0613ffcace9da2c4d19bf652bad03f2
Aljoscha Schmidt <aljoscha.schmidt@tu-darmstadt.de>
2025-11-20 11:33:33 +0100

    Removed unnecessary debug output

--- hector_controller_spawner/src/hector_controller_spawner.cpp
+++ hector_controller_spawner/src/hector_controller_spawner.cpp
@@ -575,7 +575,6 @@ int main( int argc, char **argv )
     }
     std::this_thread::sleep_for( 50ms );
   }
-  RCLCPP_INFO( node->get_logger(), "Shutting down Multi Controller Spawner node. ---------------" );
 
   node.reset();
   rclcpp::shutdown();
